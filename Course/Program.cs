f02d29fee35b3e8a282b63c8595d71cc0b4da53b
Reuel Bacellar <reubacellar@gmail.com>
2020-02-21 15:14:48 -0300

    Projeto iniciado, mostrando a mensagem Ola mundo

---
+++ Course/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Globalization;
+using System.Threading;
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá mundo!");
+        }
+    }
+}
